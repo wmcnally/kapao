f4f7d1ffaca91d5f143e3836d9c33da928ff1319
wmcnally <wmcnally@uwaterloo.ca>
2021-09-24 10:09:25 -0400

    val boxes

--- train.py
+++ train.py
@@ -351,13 +351,26 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
             ema.update_attr(model, include=['yaml', 'nc', 'hyp', 'names', 'stride', 'class_weights'])
             final_epoch = (epoch + 1 == epochs) or stopper.possible_stop
             if not noval or final_epoch:  # Calculate mAP
-                results, maps, _ = val_kp.run(data_dict,
-                                           batch_size=batch_size // WORLD_SIZE,
+                # results, maps, _ = val_kp.run(data_dict,
+                #                            batch_size=batch_size // WORLD_SIZE,
+                #                            imgsz=imgsz,
+                #                            model=ema.ema,
+                #                            dataloader=val_loader,
+                #                            compute_loss=compute_loss,
+                #                            augment=True)
+                results, maps, _ = val.run(data_dict,
+                                           batch_size=batch_size // WORLD_SIZE * 2,
                                            imgsz=imgsz,
                                            model=ema.ema,
+                                           single_cls=single_cls,
                                            dataloader=val_loader,
-                                           compute_loss=compute_loss,
-                                           augment=True)
+                                           save_dir=save_dir,
+                                           save_json=is_coco and final_epoch,
+                                           verbose=nc < 50 and final_epoch,
+                                           plots=plots and final_epoch,
+                                           callbacks=callbacks,
+                                           compute_loss=compute_loss)
+
 
             # Update best mAP
             fi = fitness(np.array(results).reshape(1, -1))  # weighted combination of [P, R, mAP@.5, mAP@.5-.95]
