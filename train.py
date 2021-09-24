6cea3202cf6e7a874f28628f3266715b3da32d76
wmcnally <wmcnally@uwaterloo.ca>
2021-09-24 09:46:48 -0400

    reduce val batch

--- train.py
+++ train.py
@@ -352,7 +352,7 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
             final_epoch = (epoch + 1 == epochs) or stopper.possible_stop
             if not noval or final_epoch:  # Calculate mAP
                 results, maps, _ = val_kp.run(data_dict,
-                                           batch_size=batch_size // WORLD_SIZE * 2,
+                                           batch_size=batch_size // WORLD_SIZE,
                                            imgsz=imgsz,
                                            model=ema.ema,
                                            dataloader=val_loader,
