38a56e9f84eaff0a1e93701f5693df031a810773
wmcnally <wmcnally@uwaterloo.ca>
2021-09-26 18:53:13 -0400

    val aug

--- train.py
+++ train.py
@@ -267,7 +267,7 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
                 f'Using {train_loader.num_workers} dataloader workers\n'
                 f"Logging results to {colorstr('bold', save_dir)}\n"
                 f'Starting training for {epochs} epochs...')
-    sys.exit()
+
     for epoch in range(start_epoch, epochs):  # epoch ------------------------------------------------------------------
         model.train()
 
@@ -352,25 +352,25 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
             ema.update_attr(model, include=['yaml', 'nc', 'hyp', 'names', 'stride', 'class_weights'])
             final_epoch = (epoch + 1 == epochs) or stopper.possible_stop
             if not noval or final_epoch:  # Calculate mAP
-                # results, maps, _ = val_kp.run(data_dict,
-                #                            batch_size=batch_size // WORLD_SIZE,
-                #                            imgsz=imgsz,
-                #                            model=ema.ema,
-                #                            dataloader=val_loader,
-                #                            compute_loss=compute_loss,
-                #                            augment=True)
-                results, maps, _ = val.run(data_dict,
-                                           batch_size=batch_size // WORLD_SIZE * 2,
+                results, maps, _ = val_kp.run(data_dict,
+                                           batch_size=batch_size // WORLD_SIZE,
                                            imgsz=imgsz,
                                            model=ema.ema,
-                                           single_cls=single_cls,
                                            dataloader=val_loader,
-                                           save_dir=save_dir,
-                                           save_json=is_coco and final_epoch,
-                                           verbose=nc < 50 and final_epoch,
-                                           plots=plots and final_epoch,
-                                           callbacks=callbacks,
-                                           compute_loss=compute_loss)
+                                           compute_loss=compute_loss,
+                                           augment=True)
+                # results, maps, _ = val.run(data_dict,
+                #                            batch_size=batch_size // WORLD_SIZE * 2,
+                #                            imgsz=imgsz,
+                #                            model=ema.ema,
+                #                            single_cls=single_cls,
+                #                            dataloader=val_loader,
+                #                            save_dir=save_dir,
+                #                            save_json=is_coco and final_epoch,
+                #                            verbose=nc < 50 and final_epoch,
+                #                            plots=plots and final_epoch,
+                #                            callbacks=callbacks,
+                #                            compute_loss=compute_loss)
 
 
             # Update best mAP
