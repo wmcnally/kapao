986af47ed8c0c4e6adcbd8488d0af76b7a2de5ec
wmcnally <wmcnally@uwaterloo.ca>
2021-09-26 18:57:58 -0400

    val aug

--- train.py
+++ train.py
@@ -217,10 +217,10 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
 
     # Process 0
     if RANK in [-1, 0]:
-        val_loader = create_dataloader(val_path, imgsz, batch_size // WORLD_SIZE * 2, gs, single_cls,
+        val_loader = create_dataloader(val_path, imgsz, batch_size // WORLD_SIZE, gs, single_cls,
                                        hyp=hyp, cache=None if noval else opt.cache, rect=False, rank=-1,
                                        workers=workers, pad=0.5,
-                                       prefix=colorstr('val: '))[0]
+                                       prefix=colorstr('val: '), kp_flip=kp_flip)[0]
 
         if not resume:
             # labels = np.concatenate(dataset.labels, 0)
