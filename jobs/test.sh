1903d82b6bc385f361d013c6a92228fb98e10384
wmcnally <wmcnally@uwaterloo.ca>
2021-09-27 15:45:16 -0400

    add sbatch file

---
+++ jobs/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=4
+#SBATCH --time=1:00:00
+#SBATCH --job-name=test
+#SBATCH --output=%x.out
+#SBATCH -p compute_full_node
+
+module load anaconda3
+source activate yolo-pose
+source deactivate yolo-pose
+source activate yolo-pose
+
+python -m torch.distributed.launch --nproc_per_node 4 train.py \
+--img 640 \
+--batch 256 \
+--epochs 10 \
+--data data/coco_kp.yaml \
+--hyp data/hyps/hyp.kp.yaml \
+--weights yolov5s.pt \
+--project runs/test \
+--name train \
+--workers 256
