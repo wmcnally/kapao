b6cf36b2b8d294f8cb62cae0f88e8b58d1e83337
wmcnally <wmcnally@uwaterloo.ca>
2021-10-02 15:03:22 -0400

    new jobs

---
+++ jobs/l_e500.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=4
+#SBATCH --time=1-00:00:00
+#SBATCH --job-name=l_e500
+#SBATCH --output=%x.out
+#SBATCH -p compute_full_node
+
+module load anaconda3
+source activate yolo-pose
+source deactivate yolo-pose
+source activate yolo-pose
+
+python -m torch.distributed.launch --nproc_per_node 4 train.py \
+--img 640 \
+--batch 128 \
+--epochs 500 \
+--data data/coco_kp.yaml \
+--hyp data/hyps/hyp.kp.yaml \
+--weights yolov5l.pt \
+--project runs/l_e500 \
+--name train \
+--workers 128 \
+--resume runs/l_e500/train/weights/last.pt
