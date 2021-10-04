987d48958ac9a5ac9ab2823bb72b9c1ee2435ca0
wmcnally <wmcnally@uwaterloo.ca>
2021-10-03 22:16:47 -0400

    new jobs

--- jobs/l_e500.sh
+++ jobs/l_e500.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=4
 #SBATCH --time=1-00:00:00
-#SBATCH --job-name=l_e500
+#SBATCH --job-name=l_e500_oa
 #SBATCH --output=%x.out
 #SBATCH -p compute_full_node
 
