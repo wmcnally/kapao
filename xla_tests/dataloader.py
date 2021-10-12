030e17307f2b4db18f88cdccc48ec4308364ae0b
wmcnally <wmcnally@uwaterloo.ca>
2021-10-12 15:02:55 -0400

    debug

--- xla_tests/dataloader.py
+++ xla_tests/dataloader.py
@@ -9,6 +9,8 @@ import torch
 import time
 import argparse
 from utils.datasets import create_dataloader, check_dataset
+import yaml
+import sys
 
 import torch_xla.core.xla_model as xm
 import torch_xla.distributed.parallel_loader as pl
@@ -17,23 +19,40 @@ import torch_xla.distributed.xla_multiprocessing as xmp
 
 def _mp_fn(index, opt):
     device = xm.xla_device()
+
     WORLD_SIZE = xm.xrt_world_size()
+    RANK = xm.get_ordinal()
 
-    print(WORLD_SIZE)
+    print(RANK)
 
-    # data_dict = check_dataset(opt.data)
-    # train_path = data_dict['train']
-    # print(train_path)
+    with open(opt.hyp) as f:
+        hyp = yaml.safe_load(f)  # load hyps dict
 
-    # train_loader, dataset = create_dataloader(train_path, imgsz, batch_size // WORLD_SIZE, gs, single_cls,
-    #                                           hyp=hyp, augment=True, cache=opt.cache, rect=opt.rect, rank=RANK,
-    #                                           workers=workers, image_weights=opt.image_weights, quad=opt.quad,
-    #                                           prefix=colorstr('train: '), kp_flip=kp_flip)
+    data_dict = check_dataset(opt.data)
+    train_path = data_dict['train']
+
+    train_loader, _ = create_dataloader(train_path, opt.imgsz, opt.batch_size // WORLD_SIZE, 32,
+                                        hyp=hyp, cache=opt.cache,
+                                        rank=RANK, workers=opt.workers, kp_flip=data_dict['kp_flip'])
+
+    train_device_loader = pl.MpDeviceLoader(train_loader, device)
+
+    for i, (imgs, targets, paths, _) in enumerate(train_device_loader):
+        if i == 10:
+            break
+        print(imgs.shape)
+
+    sys.exit()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', type=str, default='data/coco_kp.yaml', help='dataset.yaml path')
+    parser.add_argument('--hyp', type=str, default='data/hyps/hyp.kp.yaml', help='hyperparameters path')
+    parser.add_argument('--batch-size', type=int, default=64, help='total batch size for all GPUs')
+    parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='train, val image size (pixels)')
     parser.add_argument('--workers', type=int, default=8, help='maximum number of dataloader workers')
+    parser.add_argument('--cache', type=str, nargs='?', const='ram', help='--cache images in "ram" (default) or "disk"')
     opt = parser.parse_args()
 
     xmp.spawn(_mp_fn, args=(opt,), nprocs=opt.workers)
