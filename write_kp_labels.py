0d68e0c05ec1e8dcc7ec59983e211ede348f9da0
wmcnally <wmcnally@uwaterloo.ca>
2021-10-13 07:41:14 -0400

    add crowdpose

--- write_kp_labels.py
+++ write_kp_labels.py
@@ -1,4 +1,3 @@
-from pycocotools.coco import COCO
 import sys
 import os, os.path as osp
 import argparse
@@ -14,7 +13,14 @@ if __name__ == '__main__':
     parser.add_argument('--skip-no-kp', action='store_true')
     args = parser.parse_args()
 
+    assert args.data in ['coco_kp.yaml', 'crowdpose.yaml']
     obj_pose = not args.no_obj_pose
+    is_coco = 'coco' in args.data
+
+    if is_coco:
+        from pycocotools.coco import COCO
+    else:
+        from crowdposetools.coco import COCO
 
     with open(osp.join('data', args.data), 'rb') as f:
         data = yaml.safe_load(f)
@@ -22,31 +28,29 @@ if __name__ == '__main__':
     with open(osp.join('data', 'hyps', args.hyp), 'rb') as f:
         hyp = yaml.safe_load(f)
 
-    for split in ['train', 'val']:
+    splits = [osp.splitext(data[s])[0] for s in ['train', 'val', 'test'] if s in data]
+
+    assert not osp.isdir(osp.join(data['path'], 'labels')), 'Labels already generated. Remove or rename existing labels folder.'
+
+    for split in splits:
         img_paths = []
-        img_txt_path = osp.join(data['path'], '{}2017.txt'.format(split))
-        img_txt_path_debug = osp.join(data['path'], '{}2017_debug.txt'.format(split))
-        labels_path = osp.join(data['path'], 'labels/{}2017'.format(split))
-        assert not osp.isdir(labels_path), 'Labels already generated. Remove or rename existing labels folder.'
-        os.makedirs(labels_path, exist_ok=False)
-        coco = COCO(osp.join(data['path'], 'annotations/person_keypoints_{}2017.json'.format(split)))
+        img_txt_path = osp.join(data['path'], '{}.txt'.format(split))
+        img_txt_path_debug = osp.join(data['path'], '{}_debug.txt'.format(split))
+        labels_path = osp.join(data['path'], 'labels/{}'.format(split if is_coco else ''))
+        os.makedirs(labels_path, exist_ok=True)
+        coco = COCO(data['path'] + data['annotations_prefix'] + '{}.json'.format(split))
 
         for count, id in enumerate(coco.anns.keys()):
             a = coco.anns[id]
 
-            if split == 'train':
-                if a['iscrowd']:
+            if 'train' in split:
+                if is_coco and a['iscrowd']:
                     continue
                 if args.skip_no_kp and ((np.sum(a['keypoints'][2::3]) == 0) or (a['num_keypoints'] == 0)):
                     continue
 
             if a['image_id'] in coco.imgs:
                 img_info = coco.imgs[a['image_id']]
-
-                # img_path = osp.join(data['path'], 'images', '{}2017'.format(split), img_info['file_name'])
-                # if img_path not in img_paths:
-                #     img_paths.append(img_path)
-
                 img_h, img_w = img_info['height'], img_info['width']
                 x, y, w, h = a['bbox']
                 xc, yc = x + w/2, y + h/2
@@ -57,7 +61,7 @@ if __name__ == '__main__':
 
                 keypoints = np.array(a['keypoints']).reshape([-1, 3])
 
-                with open(osp.join(labels_path, '{}.txt'.format(img_info['file_name'].split('.')[0])), 'a') as f:
+                with open(osp.join(labels_path, '{}.txt'.format(osp.splitext(img_info['file_name'])[0])), 'a') as f:
                     # write person object
                     s = '{} {:.6f} {:.6f} {:.6f} {:.6f}'.format(0, xc, yc, w, h)
                     if obj_pose:
@@ -88,28 +92,12 @@ if __name__ == '__main__':
             if (count + 1) % 1000 == 0:
                 print('{} {}/{}'.format(split, count + 1, len(coco.anns.keys())))
 
-        # if split == 'train':
-        #     print('Writing image paths...')
-        #     with open(img_txt_path, 'w') as f:
-        #         for img_path in img_paths:
-        #             f.write(img_path + '\n')
-        #
-        #     with open(img_txt_path_debug, 'w') as f:
-        #         for i, img_path in enumerate(img_paths):
-        #             if i == 128:
-        #                 break
-        #             f.write(img_path + '\n')
-        # else:
-
-        print('Writing image paths...')
         with open(img_txt_path, 'w') as f:
             for img_info in coco.imgs.values():
-                f.write(osp.join(data['path'], 'images', '{}2017'.format(split), img_info['file_name']) + '\n')
+                f.write(osp.join(data['path'], 'images', '{}'.format(split if is_coco else ''), img_info['file_name']) + '\n')
 
         with open(img_txt_path_debug, 'w') as f:
             for i, img_info in enumerate(coco.imgs.values()):
                 if i == 128:
                     break
-                f.write(osp.join(data['path'], 'images', '{}2017'.format(split), img_info['file_name']) + '\n')
-
-
+                f.write(osp.join(data['path'], 'images', '{}'.format(split if is_coco else ''), img_info['file_name']) + '\n')
