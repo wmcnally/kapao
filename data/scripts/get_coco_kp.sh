c558b9a798a14b7008d08378a1f6ddc8cba7972f
wmcnally <wmcnally@uwaterloo.ca>
2021-10-28 09:07:21 -0400

    download test images

--- data/scripts/get_coco_kp.sh
+++ data/scripts/get_coco_kp.sh
@@ -20,7 +20,7 @@ url=http://images.cocodataset.org/zips/
 f1='train2017.zip' # 19G, 118k images
 f2='val2017.zip'   # 1G, 5k images
 f3='test2017.zip'  # 7G, 41k images (optional)
-for f in $f1 $f2; do
+for f in $f1 $f2 $f3; do
   echo 'Downloading' $url$f '...'
   curl -L $url$f -o $f && unzip -q $f -d $d && rm $f &
 done
