c6e2c6919bd6edd7fc82162898c9592e3f80bfe3
wmcnally <wmcnally@uwaterloo.ca>
2021-11-02 08:59:15 -0400

    new job

---
+++ jobs/cp_s_e300_p6_ab.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=4
+#SBATCH --time=6:00:00
+#SBATCH --job-name=cp_s_e300_p6_ab
+#SBATCH --output=%x.out
+#SBATCH -p compute_full_node
+
+module load anaconda3
+source activate yolo-pose
+source deactivate yolo-pose
+source activate yolo-pose
+
+python -m torch.distributed.launch --nproc_per_node 4 train.py \
+--img 1280 \
+--batch 128 \
+--epochs 500 \
+--data data/crowdpose.yaml \
+--hyp data/hyps/hyp.kp-p6.yaml \
+--val-scales 1 \
+--val-flips -1 \
+--weights yolov5s6.pt \
+--project runs/crowdpose/cp_s_e300_p6_ab \
+--name train \
+--workers 128 \
+--noval \
+--autobalance
