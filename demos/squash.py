df7aa84a92600ea849ac1d00cdfc10b95e80c20e
wmcnally <wmcnally@uwaterloo.ca>
2021-11-04 12:44:07 -0400

    comments

--- demos/squash.py
+++ demos/squash.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     parser.add_argument('--data', type=str, default='data/coco-kp.yaml')
     parser.add_argument('--imgsz', type=int, default=1280)
     parser.add_argument('--weights', default='yolopose_s_coco.pt')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or cpu')
     parser.add_argument('--half', action='store_true')
     parser.add_argument('--conf-thres', type=float, default=0.5, help='confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
